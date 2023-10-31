08a34f4fabb105a9d183df94c78e54289959aa14
samson1035 <stockmann719@gmail.com>
2023-10-31 13:43:03 -0500

    broadcast test works

--- enigmashowdown/__init__.py
+++ enigmashowdown/__init__.py
@@ -2,19 +2,20 @@
 
 import zmq
 
-"""
-Common classes to look at (for now):
 
-    ServerTest - class to run to test python client
-    ZeroMqServerManager - contains server logic for creating REP socket (reply)
-    EnigmaShowdownConstants - contains port nums
-    ClientCommunicationTest - Kotlin equivalent of python client file
-    ZeroMqRequestClient - should eventually write in python (end goal); allows for easy communication with server
-    ConnectRequest - class that is able to be serialized into json
+def client_test():
+    """
+    Common classes to look at (for now):
 
-"""
+        ServerTest - class to run to test python client
+        ZeroMqServerManager - contains server logic for creating REP socket (reply)
+        EnigmaShowdownConstants - contains port nums
+        ClientCommunicationTest - Kotlin equivalent of this function
+        ZeroMqRequestClient - should eventually write in python (end goal); allows for easy communication with server
+        ConnectRequest - class that is able to be serialized into json
 
-def main():
+    """
+
     request_json = """
 {
     "type": "connect-request",
@@ -31,11 +32,37 @@ def main():
     for _ in range(10):
         socket.send(request_json.encode('utf-8'))
 
-        #  Get the reply.
+        #  Get the reply
+        message = socket.recv().decode('utf-8')
+        print(message)
+    
+    return
+
+
+def broadcast_test():
+    """
+    Common classes to reference:
+        BroadcastServerTest - class to run that python broadcast client will comm with
+        ZeroMqBroadcastManager - contains server logic for creating PUB socket (publish)
+        ClientBroadcastListenTest - Kotlin equivalent of this function
+        TODO: implement ZeroMqBroadcastReceiver function in ClientBroadcastReceiver
+            has logic for creating SUB (subscribe) socket
+    """
+
+    context = zmq.Context()
+
+    #  Socket to talk to server
+    socket = context.socket(zmq.SUB)
+    socket.connect("tcp://localhost:31878")
+
+    socket.subscribe("")
+
+    for _ in range(10):
         message = socket.recv().decode('utf-8')
         print(message)
 
     return
 
 if __name__=="__main__":
-    main()
+    # client_test()
+    broadcast_test()
